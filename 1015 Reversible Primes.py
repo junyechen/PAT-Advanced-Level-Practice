70d5588193a7c4dd6266dd01873c465287275e21
俊晔 陈 <chenjunyeword@outlook.com>
2019-10-10 21:37:21 +0800

    1015 Reversible Primes

--- 1015 Reversible Primes.py
+++ 1015 Reversible Primes.py
@@ -26,26 +26,39 @@ Yes
 No
 """
 
-prime = [1] * 1048577
+###########################################################
+"""
+本题简单
+用python建立素数表，数字范围为＜10^5，则进制逆转后，最大为16^4-1，因n若转成16进制，则最多4位
+进制转换非常简单
+注意本身数字是否为素数！
+"""
+###########################################################
+
+prime = [1] * 65536
 prime[0] = 0
 prime[1] = 0
-for i in [4, 1048578, 2]:
+for i in range(4, 65536, 2):
     prime[i] = 0
-for i in [3, 1048578, 2]:
+for i in range(3, 65536, 2):
     if prime[i] == 1:
-        for j in [i + i * 2, 1048578, i * 2]:
-            prime[j] = 0
+        if i * 3 < 65536:
+            for j in range(i*3, 65536, i * 2):
+                prime[j] = 0
 while True:
     line = input()
     if line[0] == '-':
         break
     else:
         num, radix = [int(i) for i in line.split()]
-        num_ = 0
-        while num != 0:
-            num_ = num_ * radix + num % radix
-            num = num // radix
-        if prime[num_] == 1:
-            print("Yes")
+        if prime[num] == 1:
+            num_ = 0
+            while num != 0:
+                num_ = num_ * radix + num % radix
+                num = num // radix
+            if prime[num_] == 1:
+                print("Yes")
+            else:
+                print("No")
         else:
             print("No")
